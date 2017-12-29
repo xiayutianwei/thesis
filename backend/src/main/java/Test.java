83bbdce23119d9f8a227dc20f598e2171cf2959b
liuziwei <liuziwei@neotel.com.cn>
2017-12-29 17:11:43 +0800

    init

---
+++ backend/src/main/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * Created by liuziwei on 2017/12/29.
+ */
+import java.util.*;
+public class Test {
+
+    public static void main(String[] args){
+        Map<Integer,Integer> a=new TreeMap<Integer,Integer>();
+    }
+}
--- backend/src/main/scala/thesis/core/Master.scala
+++ backend/src/main/scala/thesis/core/Master.scala
@@ -1,8 +1,21 @@
 package thesis.core
 
+import akka.actor.Actor.Receive
+import akka.actor._
 /**
   * Created by liuziwei on 2017/12/26.
   */
 object Master {
 
+}
+
+class Master extends Actor {
+
+
+  @scala.throws[Exception](classOf[Exception])
+  override def preStart(): Unit = super.preStart()
+
+  override def receive: Receive = ???
+
+  def idle
 }
